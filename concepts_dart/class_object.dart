ead0e50fcbbf3e38f4a2ea068ae8ffbb8aab23d6
heisenberg0042 <arpitparekh0042@gmail.com>
2022-03-10 11:47:41 +0530

    dgdgd

---
+++ concepts_dart/class_object.dart
@@ -0,0 +1,36 @@
+
+class ClassName{
+
+  // properties // fields
+
+  // getter setter
+
+  // constructor
+
+  // functiion
+
+}
+
+class Student{
+
+  var name;
+  var address;
+
+  showInfo(){
+
+    print("name is $name \naddress is $address");
+
+  }
+
+}
+
+void main(List<String> args) {
+  
+  Student s = new Student();
+
+  s.name="pratap";
+  s.address="surat";
+
+  s.showInfo();
+
+}
---
+++ concepts_dart/functions.dart
@@ -0,0 +1,26 @@
+
+display(){
+
+  print("This is Display method");
+}
+
+int sum(int a,int b){
+
+  return a+b;
+}
+
+int data(){
+
+  return 23;
+}
+
+void main(List<String> args) {
+
+    display();
+
+    print(sum(12, 34));
+
+    print(data());
+
+    
+}
