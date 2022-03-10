6cdf7a35bce33a617e5f39ad7e473d9868d13e15
heisenberg0042 <arpitparekh0042@gmail.com>
2022-03-10 11:50:53 +0530

    dgfdg

---
+++ concepts_dart/anonymous_function.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  
+    // lambda
+    // anonymous
+    // closure
+
+    var list = [12,13,14,15];
+
+    list.forEach((element) {   // anonymous function inside foreach function
+      
+        print(element);
+    });
+
+    
+
+}
