3fa7fffb6c267abebbdae73950ba51d42eff6f6f
heisenberg0042 <arpitparekh0042@gmail.com>
2022-03-11 12:46:27 +0530

    ffg

--- concepts_dart/copy_constructor.dart
+++ concepts_dart/copy_constructor.dart
@@ -1,5 +1,35 @@
+class Computer{
+
+  Computer(this.brand,this.price);
+
+  var brand;
+  var price;
+
+  Computer.copy(Computer c){   // using another constructor
+
+    this.brand = c.brand;
+    this.price = c.price;
+
+  }
+
+  void display(){
+
+    print("brand : $brand\nprice : $price");
+  }
+
+}
+
 void main(List<String> args) {
   
-  
+    // make copy if an constrcutor
+
+    var c = new Computer("asus", 12000);
+
+    c.display();
+
+    var c1 = new Computer.copy(c);   // make a copy of an object using copy constructor
+
+    c1.display();
+
 
 }
