b53fe5052b53ca02127dc2c7e4e09e978a178522
heisenberg0042 <arpitparekh0042@gmail.com>
2022-03-11 12:34:31 +0530

    dfgfg

--- concepts_dart/factory_constructor.dart
+++ concepts_dart/factory_constructor.dart
@@ -1,7 +1,74 @@
 
 class Database{
 
- Database(this.size,this.no_row,this.no_column);
+
+  /////////   /////////////  first way   ////////////////////
+  
+
+//  Database(this.size,this.no_row,this.no_column);
+
+// factory Database(String database){
+   
+//     if(database=="mysql"){
+
+//       return new Database.mysql(60, 20, 5);
+//     }else if(database=="mongoDB"){
+
+//       return Database.mongoDB(80, 50, 7);
+
+//     }else if(database == "mssql"){
+
+//       return Database.msSql(20, 5, 2);
+//     }else{
+
+//       throw ArgumentError("wrong argument");   // factory constructor does not return null
+//     }
+  
+//  }
+
+//  var size;
+//  var no_row;
+//  var no_column;
+
+//  Database.mysql(this.size,this.no_row,this.no_column){
+
+//    print("This is mysql database");
+//    print("size : $size\nno of row : $no_row\nno of coliumn : $no_column");
+//  }
+//  Database.mongoDB(this.size,this.no_row,this.no_column){
+
+//    print("This is mongo database");
+//    print("size : $size\nno of row : $no_row\nno of coliumn : $no_column");
+//  }
+//  Database.msSql(this.size,this.no_row,this.no_column){
+
+//    print("This is mssql database");
+//    print("size : $size\nno of row : $no_row\nno of coliumn : $no_column");
+//  }
+
+
+
+/////////////////////////////////////  second way  /////////////////////////////////
+
+
+factory Database(String database,var size,var no_row,var no_column){
+   
+    if(database=="mysql"){
+
+      return new Database.mysql(size, no_row, no_column);
+    }else if(database=="mongoDB"){
+
+      return Database.mongoDB(size, no_row, no_column);
+
+    }else if(database == "mssql"){
+
+      return Database.msSql(size, no_row, no_column);
+    }else{
+
+      throw ArgumentError("wrong argument");   // factory constructor does not return null
+    }
+  
+ }
 
  var size;
  var no_row;
@@ -10,14 +77,17 @@ class Database{
  Database.mysql(this.size,this.no_row,this.no_column){
 
    print("This is mysql database");
+   print("size : $size\nno of row : $no_row\nno of coliumn : $no_column");
  }
  Database.mongoDB(this.size,this.no_row,this.no_column){
 
    print("This is mongo database");
+   print("size : $size\nno of row : $no_row\nno of coliumn : $no_column");
  }
  Database.msSql(this.size,this.no_row,this.no_column){
 
    print("This is mssql database");
+   print("size : $size\nno of row : $no_row\nno of coliumn : $no_column");
  }
 
 }
@@ -40,6 +110,8 @@ void main(List<String> args) {
     //   Database d = new Database.msSql(40, 56, 6);
     // }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
+     // make main function consice    ///////////////////////////////////////////////////////////////////
+    
+    Database d = new Database("mysql");
 
 }
