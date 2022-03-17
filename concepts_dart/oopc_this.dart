beae201f3a7e31319aee22b7cab263bcf8bdb7f5
arpitparekh <arpitparekh9@gmail.com>
2022-03-17 23:02:56 +0530

    dfdf

--- concepts_dart/oopc_this.dart
+++ concepts_dart/oopc_this.dart
@@ -1,80 +1,14 @@
- String Function(int) display(){
 
-  return(int x){
+class Room{
 
-      return "inner and $x";
-  };
-}
+  var roommate_name;
+  var isClothOn;
 
-void Function() show(){
-
-  return(){
-
-    print("Hello");
-
-  };
+  
 
 }
-
-String Function() Function() Function() data(){
-
-  return(){
-
-    return(){
-
-      return(){ 
-
-        return "dart";
-      };
-    };
-  };
-}
-
-String giveString(){
-
-    return "this is giveString Function";
-
-}
-
-String give(String Function()){
-
-    return Function();
-}
-
-int Function() sum(int a,int b){
-  return (){
-
-    return a+b;
-  };
-}
-int Function() sub(int a,int b){
-  return(){
-
-    return a-b;
-
-  };
-}
-
-int mix(int Function(),int Function2()){
-
-    return Function()+Function2();
-
-}
-
 void main(List<String> args) {
 
-  var a = display();
-  var b = show();
 
-  print(a.runtimeType);
-  print(b.runtimeType);
-
-  print(a(20));
-
-  b();
-
-  print(give(giveString));
-
-  print(mix( sum(10,20), sub(10,20) ));
 
 }
